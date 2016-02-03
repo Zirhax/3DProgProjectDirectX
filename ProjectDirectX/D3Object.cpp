baddb90b07bc6a9dc5fb4f42e4f2955bea603571
Pistonnoob <kimeld@hotmail.com>
2016-02-03 12:31:26 +0100

    Worked on LoadModel obj

--- ProjectDirectX/D3Object.cpp
+++ ProjectDirectX/D3Object.cpp
@@ -225,11 +225,15 @@ void D3Object::ReleaseTexture()
 bool D3Object::LoadModel(char * fileName)
 {
 	ifstream fileIn;
-	const int READSIZE = 20;
-	string special, line, line2;
-
+	string special = "", line = "", line2 = "";
+	istringstream inputString;
 	vector<Vector3> vertices;
-	Vector3 vtx;
+	vector<Vector3> normals;
+	vector<Vector2> UV;
+	vector<Vector3[3]> indices;
+	vector<VertexModel> vertexData;
+	Vector3 vtx = { 0, 0, 0 }, vn = { 0, 0, 0 };
+	Vector2 vt = { 0, 0 };
 	
 	fileIn.open(fileName, ios::in);
 	if (!fileIn.is_open())
@@ -239,6 +243,46 @@ bool D3Object::LoadModel(char * fileName)
 
 	while (std::getline(fileIn, line2))
 	{
+		inputString.str(line2);
+		if (line2.substr(0, 2) == "v ")
+		{
+			// Vertex Position
+			inputString >> special >> vtx.x >> vtx.y >> vtx.z;
+			vertices.push_back(vtx);
+		}
+		else if (line2.substr(0, 2) == "vt")
+		{
+			// Vertex UV
+			inputString >> special >> vt.x >> vt.y;
+			UV.push_back(vt);
+		}
+		else if (line2.substr(0, 2) == "vn")
+		{
+			// Vertex Normal
+			inputString >> special >> vn.x >> vn.y >> vn.z;
+			normals.push_back(vn);
+		}
+		else if (line2.substr(0, 2) == "g ")
+		{
+			//Group name
+		}
+		else if (line2.substr(0, 7) == "mtllib ")
+		{
+			//Material name
+		}
+		else if (line2.substr(0, 2) == "f ")
+		{
+			//Vertex Normal Indices in format f v1/vt1/vn1
+			char temp[512];
+			strncpy(temp, line2.c_str(), sizeof(temp));
+			temp[sizeof(temp) - 1] = 0;
+			char* pos = strstr(temp, "f ");
+			Vector3 faceIndices[3];
+			sscanf(pos, "%s %i/%i/%i %i/%i/%i %i/%i/%i\n", &special, &faceIndices[0].x, &faceIndices[0].y, &faceIndices[0].z,
+				&faceIndices[1].x, &faceIndices[1].y, &faceIndices[1].z,
+				&faceIndices[2].x, &faceIndices[2].y, &faceIndices[2].z);
+			indices.push_back(faceIndices);
+		}
 	}
 
 
