db36144ac69809770903afa1a5deeda89ee3ad5b
Pistonnoob <kimeld@hotmail.com>
2016-02-09 22:20:48 +0100

    Updated Factory CreateFromObj

--- ProjectDirectX/ObjectFactory.cpp
+++ ProjectDirectX/ObjectFactory.cpp
@@ -20,11 +20,94 @@ vector<D3Object*> ObjectFactory::CreateFromFile(char * fileName, FactoryObjectFo
 
 void ObjectFactory::CreateFromFile(char * fileName, FactoryObjectFormat objectFormat, vector<D3Object*> &storeIn)
 {
-
+	switch (objectFormat)
+	{
+	case OBJ:
+	case OBJ_LH:
+		this->CreateFromObj(fileName, storeIn, 1);
+			break;
+	case OBJ_RH:
+		this->CreateFromObj(fileName, storeIn, -1);
+		break;
+	case TXT:
+		break;
+	default:
+		break;
+	}
 }
 
-void ObjectFactory::CreateFromObj(char * fileName, vector<D3Object*>& storeIn, int index)
+bool ObjectFactory::CreateFromObj(char * fileName, vector<D3Object*>& storeIn, int invert)
 {
+	ifstream fileIn;
+	string special = "", line = "", line2 = "";
+	char specialChar[10];
+	istringstream inputString;
+	vector<Vector3> vertices;
+	vector<Vector3> normals;
+	vector<Vector2> UV;
+	vector<VertexModel> vertexData;
+	Vector3 vtx = { 0, 0, 0 }, vn = { 0, 0, 0 };
+	Vector2 vt = { 0, 0 };
+
+	char temp[512];
+
+	fileIn.open(fileName, ios::in);
+	if (!fileIn.is_open())
+	{
+		return false;
+	}
+
+	while (std::getline(fileIn, line2))
+	{
+		strncpy(temp, line2.c_str(), sizeof(temp));
+		temp[sizeof(temp) - 1] = 0;
+
+		inputString.str(line2);
+		if (line2.substr(0, 2) == "v ")
+		{
+			// Vertex Position
+			sscanf(temp, "%s %f %f %f\n", specialChar, &vtx.x, &vtx.y, &vtx.z);
+			vtx.z *= invert;
+			//inputString >> special >> vtx.x >> vtx.y >> vtx.z;
+			vertices.push_back(vtx);
+		}
+		else if (line2.substr(0, 2) == "vt")
+		{
+			// Vertex UV
+			sscanf(temp, "%s %f %f\n", specialChar, &vt.x, &vt.y);
+			//inputString >> special >> vt.x >> vt.y;
+			UV.push_back(vt);
+		}
+		else if (line2.substr(0, 2) == "vn")
+		{
+			// Vertex Norma
+			sscanf(temp, "%s %f %f %f\n", specialChar, &vn.x, &vn.y, &vn.z);
+			//inputString >> special >> vn.x >> vn.y >> vn.z;
+			normals.push_back(vn);
+		}
+		else if (line2.substr(0, 2) == "g ")
+		{
+			//Group name
+		}
+		else if (line2.substr(0, 7) == "mtllib")
+		{
+			//Material name
+		}
+		else if (line2.substr(0, 2) == "f ")
+		{
+			//Vertex Normal Indices in format f v1/vt1/vn1
+			struct IndexStruct { int v; int vt; int vn; } faceIndices[3];
+			sscanf(temp, "%s %i/%i/%i %i/%i/%i %i/%i/%i\n", specialChar,
+				&faceIndices[0].v, &faceIndices[0].vt, &faceIndices[0].vn,
+				&faceIndices[1].v, &faceIndices[1].vt, &faceIndices[1].vn,
+				&faceIndices[2].v, &faceIndices[2].vt, &faceIndices[2].vn);
+			//VertexModel tempModelData = {vertices[&faceIndices[0].v - 1], UV[&faceIndices[0].v], normals[&faceIndices[0].vn]};
+			for (int i = 0; i < 3; i++)
+				vertexData.push_back({ vertices[faceIndices[i].v - 1], UV[faceIndices[i].vt - 1], normals[faceIndices[i].vn - 1] });
+
+		}
+	}
+	fileIn.close();
 }
 
 
