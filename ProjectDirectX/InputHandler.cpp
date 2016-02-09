01ce504aa06ec62527ee40a86dfd51805097de7f
Pistonnoob <kimeld@hotmail.com>
2016-02-09 21:32:34 +0100

    IsKeyPressed now returns if the key was pressed.

--- ProjectDirectX/InputHandler.cpp
+++ ProjectDirectX/InputHandler.cpp
@@ -146,21 +146,27 @@ bool InputHandler::Frame()
 	return true;
 }
 
-bool InputHandler::IsEscapePressed(int keyboardScanCode)
+bool InputHandler::IsEscapePressed()
 {
 	// Do a bitwise and on the keyboard state to check if the escape key is currently being pressed.
 	if (this->m_keys[DIK_ESCAPE] & 0x80)
 	{
 		return true;
-		DIKEYBOARD_0;
 	}
 
 	return false;
 }
 
-bool InputHandler::IsKeyPressed()
+bool InputHandler::IsKeyPressed(int keyboardScanCode)
 {
+	bool result = false;
 
+	if (this->m_keys[keyboardScanCode])
+	{
+		return true;
+	}
+
+	return result;
 }
 
 bool InputHandler::IsKeyDown(unsigned int key)
